8fc5451f7840ccb13f26f1af4fa8155558ccb3dd
Thai Quang Hung <thaiquanghungit@gmail.com>
2019-09-26 12:09:29 +0700

    Update 709
    Update 709

--- 709/709-content.php
+++ 709/709-content.php
@@ -10,20 +10,21 @@
             include ('libs/lessc.inc.php');
         }
         $less = new lessc;
-        $less->compileFile('less/854.less', 'css/854.css');
+        $less->compileFile('less/709.less', 'css/709.css');
         ?>
-        <link rel="stylesheet" href="css/854.css"> 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="./css/709.css"> 
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/font-awesome.min.css">
 
           
 
         <script src="js/jquery-1.11.0.min.js"></script>
         <script src="js/bootstrap.min.js"></script> 
+        <script src="js/app.js"></script> 
+        
     </head>
-    <body>
-        <div class="type-854">
-		<!-- BEGIN TYPE-854 -->
+    <div class="type-709">
+		<!-- BEGIN TYPE-709 -->
             <div id="pattent" class="slidelip white-background">  
                 <div class="container container-modify margin-auto header">
                     <!--MEGA MENU-->
@@ -285,42 +286,6 @@
                     </div>
                 </div>
             </div>
-			<!-- END TYPE-854 -->
+			<!-- END TYPE-709 -->
         </div> 
-    </body>
-
-    <script>
-        $(document).ready(function () {
-            //  DROPDOWN
-            if ($(window).width() > 728) {
-                $(".dropdown2").hover(
-                        function () {
-                            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).fadeIn("600");
-                            $(this).toggleClass('open');
-                        },
-                        function () {
-                            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).fadeOut("600");
-                            $(this).toggleClass('open');
-                        }
-                );
-            }
-            if ($(window).width() < 728) {
-                var i = 0;
-                $(".dropdown2", this).click(function () {
-                    if (i % 2 !== 0) {
-                        $('span.opener', this).removeClass("close");
-                        $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).fadeOut("600");
-                        $(this).toggleClass('open');
-                    } else {
-                        $('span.opener', this).addClass("close").css({'font-size': '28px', 'color': '#009900', 'opacity': '1'});
-                        $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).fadeIn("600");
-                        $(this).toggleClass('open');
-                    }
-                    i++;
-
-                });
-            }
-        });
-
-    </script>
 </html>
