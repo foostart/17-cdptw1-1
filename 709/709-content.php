27b576e04c442e629bc0bfcc85d7fbec6ba06c73
Thai Quang Hung <thaiquanghungit@gmail.com>
2019-09-26 15:04:22 +0700

    Chưa hoàn thành
    Chưa hoàn thành

--- 709/709-content.php
+++ 709/709-content.php
@@ -14,9 +14,7 @@
         ?>
         <link rel="stylesheet" href="./css/709.css"> 
         <link rel="stylesheet" href="./css/bootstrap.min.css">
-        <link rel="stylesheet" href="./css/font-awesome.min.css">
-
-          
+        <link rel="stylesheet" href="./css/font-awesome.min.css">          
 
         <script src="js/jquery-1.11.0.min.js"></script>
         <script src="js/bootstrap.min.js"></script> 
@@ -31,33 +29,23 @@
                     <div class="menu-container">
 
                         <div class="mega-menu">
-                            <div class="mobileMenuTitle">Menu</div>
                             <ul class="mega-menu-ul">
-                                <li class="dropdown dropdown2 mega-dropdown mega-menu-item hasChildren">
-                                    <a href="list" class="menu-item dropdown-toggle">
-                                        <span>Women</span> 
-                                    </a>	
-                                    <span  class="opener"></span>
-                                    <div class="margin-auto dropdown-menu mega-dropdown-menu" >
+                                <li class="dropdown dropdown2 mega-dropdown mega-menu-item top-dau">
+                                    <a href="#" class="menu-item dropdown-toggle" data-toggle="dropdown">
+                                        <span>WOMAN</span>
+                                    </a>				
+                                    <span class="opener"></span>
+                                    <div class="container margin-auto dropdown-menu mega-dropdown-menu hung-a hung-hien">
                                         <div class="col-md-3 col-sm-3 col-xs-3 mobile-hide">
-                                            <ul class="first-item">
-                                                <li class="dropdown-header">Men Collection</li>  
-                                                <div class="item active">
-                                                    <a href="#">
-                                                        <img src="images/menu/dress-anna-sui-ambrosia-lace.jpg" class="img-responsive" alt="product 1">
-                                                    </a> 
-                                                </div>
-
-                                                <li class="divider"></li>
-                                                <li>
-                                                    <a href="#">Dress Anna Sui....</a>
-                                                    <span class="price">$309.00</span>
-                                                </li>
-                                            </ul>
+                                            <div class="box-img-left">
+                                                <a href="#" class="banner-left">
+                                                    <img src="images/menu/banner-left.jpg" class="img-responsive" alt="product 1">
+                                                </a>   
+                                            </div>
                                         </div>
-                                        <div class="col-md-9 col-sm-9 col-xs-9 full-width-mn">
-                                            <ul class="full-width women" >
-                                                <li class="col-md-4 col-sm-4 col-xs-4">
+                                        <div class="col-md-6 col-sm-6 col-xs-6 full-width-mn">
+                                            <ul class="full-width men">
+                                                <li class="col-md-6 col-sm-6 col-xs-6">
                                                     <ul>
                                                         <li class="dropdown-header">Features</li>
                                                         <li><a href="#">Auto Carousel</a></li>
@@ -67,7 +55,7 @@
                                                         <li class="divider"></li> 
                                                     </ul>
                                                 </li>
-                                                <li class="col-md-4 col-sm-4  col-xs-4">
+                                                <li class="col-md-6 col-sm-6 col-xs-6">
                                                     <ul>
                                                         <li class="dropdown-header">Plus</li>
                                                         <li><a href="#">Navbar Inverse</a></li>
@@ -76,39 +64,50 @@
                                                         <li><a href="#">Primary Buttons & Default</a></li>							
                                                     </ul>
                                                 </li>
-                                                <li class="col-md-4 col-sm-4 col-xs-4">
+                                                <li class="col-md-6 col-sm-6 col-xs-6">
                                                     <ul>
-                                                        <li class="dropdown-header">Much more</li>
-                                                        <li><a href="#">Easy to Customize</a></li>
-                                                        <li><a href="#">Calls to action</a></li>
-                                                        <li><a href="#">Custom Fonts</a></li>
-                                                        <li><a href="#">Slide down on Hover</a></li>                         
+                                                        <li class="dropdown-header">Features</li>
+                                                        <li><a href="#">Auto Carousel</a></li>
+                                                        <li><a href="#">Carousel Control</a></li>
+                                                        <li><a href="#">Left & Right Navigation</a></li>
+                                                        <li><a href="#">Four Columns Grid</a></li>
+                                                        <li class="divider"></li> 
                                                     </ul>
                                                 </li>
-                                                <li class="col-md-4 col-sm-4 col-xs-4">
+                                                <li class="col-md-6 col-sm-6 col-xs-6">
                                                     <ul>
-                                                        <li class="dropdown-header">Much more</li>
-                                                        <li><a href="#">Easy to Customize</a></li>
-                                                        <li><a href="#">Calls to action</a></li>
-                                                        <li><a href="#">Custom Fonts</a></li>
-                                                        <li><a href="#">Slide down on Hover</a></li>                         
+                                                        <li class="dropdown-header">Plus</li>
+                                                        <li><a href="#">Navbar Inverse</a></li>
+                                                        <li><a href="#">Pull Right Elements</a></li>
+                                                        <li><a href="#">Coloured Headers</a></li>                            
+                                                        <li><a href="#">Primary Buttons & Default</a></li>							
                                                     </ul>
-                                                </li> 
+                                                </li>
+
                                             </ul>
                                         </div>
-                                        <div class="col-md-9 col-sm-12  col-xs-12 mobile-hide clearfix">
-                                            <a>
-                                                <img src="images/menu/banner-bottom.jpg" class="img-responsive">
-                                            </a>
+                                        <div class="col-md-3 col-sm-3 col-xs-3 mobile-hide">
+                                            <ul class="item-men-right">
+                                                <li class="dropdown-header">Men Collection</li>  
+                                                <a href="#" class="banner-right">
+                                                    <img src="images/menu/dress-anna-sui-ambrosia-lace.jpg" class="img-responsive" alt="product 1">
+                                                </a> 
+                                                <li class="divider"></li>
+                                                <li>
+                                                    <a href="#">View all Collection 
+
+                                                    </a >
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>	
                                 </li>
                                 <li class="dropdown dropdown2 mega-dropdown mega-menu-item">
                                     <a href="#" class="menu-item dropdown-toggle" data-toggle="dropdown">
-                                        <span>Men</span>
+                                        <span>MEN</span>
                                     </a>				
                                     <span  class="opener"></span>
-                                    <div class="container margin-auto dropdown-menu mega-dropdown-menu" >
+                                    <div class="container margin-auto dropdown-menu mega-dropdown-menu hung-a hung-hien">
                                         <div class="col-md-3 col-sm-3 col-xs-3 mobile-hide">
                                             <div class="box-img-left">
                                                 <a href="#" class="banner-left">
@@ -176,10 +175,9 @@
                                     </div>	
                                 </li>
 
-                                <li class="dropdown dropdown2 mega-dropdown mega-menu-item">
+                                <li class="dropdown dropdown2 mega-dropdown mega-menu-item ">
                                     <a href="blog" class="menu-item">
-                                        <span>Blog</span>
-
+                                        <span>BLOG</span>
                                     </a> 
                                     <span  class="opener"></span>
                                 </li>
@@ -189,7 +187,7 @@
                                     </a>
                                     <span  class="opener"></span>
 
-                                    <div class="container margin-auto dropdown-menu mega-dropdown-menu" >
+                                    <div class="container margin-auto dropdown-menu mega-dropdown-menu hung-hien">
                                         <div class="align-center full-width">
                                             <h2>Alysum -  is the universal theme, with the pleasant and clean design.</h2>
                                             <p>With this theme you can easily create your unique shop, choosing any colors & over 500 Google fonts. </p>
@@ -221,10 +219,10 @@
 
                                 <li class="dropdown dropdown2 mega-dropdown mega-menu-item">
                                     <a href="#"class="menu-item">
-                                        <span>Brands</span>
+                                        <span>BRANDS</span>
                                     </a>
                                     <span  class="opener"></span>
-                                    <ul class="container margin-auto dropdown-menu mega-dropdown-menu" >
+                                    <ul class="container margin-auto dropdown-menu mega-dropdown-menu hung-hien">
                                         <li class="col-md-2 col-sm-2 brand">
                                             <img src="images/menu/1-manu_alysum.jpg" class="img-responsive">
                                         </li> 
@@ -266,7 +264,7 @@
 
                                 <li class="dropdown dropdown2 mega-dropdown mega-menu-item">
                                     <a href="#"class="menu-item">
-                                        <span>Demos</span>
+                                        <span>DEMOS</span>
                                     </a>
                                     <span  class="opener"></span>
                                     <ul class="dropdown-menu mega-dropdown-menu demos" >
@@ -283,6 +281,9 @@
 
                         </div><!-- /.nav-collapse -->
 
+                    </div>
+                    <div class="test">
+                        <p>Hung Test Menu de len</p>
                     </div>
                 </div>
             </div>
